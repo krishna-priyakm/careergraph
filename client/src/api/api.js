6c111a921774ad755880ba29695c7104e083a18a
krishnapriya <krishnapriyamkkp@gmail.com>
2026-08-18 12:34:05 +0530

    Connect frontend to deployed backend

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://careergraph-api-1jat.onrender.com/api";
 
 export const getPeople = async () => {
   const response = await fetch(`${API_BASE_URL}/people`);
