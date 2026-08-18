f27339fd9490cd970572b2a0df9e1b8f8f501e99
krishnapriya <krishnapriyamkkp@gmail.com>
2026-08-18 12:01:59 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -32,7 +32,8 @@ app.get("/api/health", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
+app.listen(PORT, "0.0.0.0", async () => {
   console.log(`🚀 Server running on port ${PORT}`);
+
   await connectDatabase();
 });
